06705dfe1dac4fb4a1a8771c04d06a9e2fbf2187
Thiago Emanoel <thiagoemanoel448@gmail.com>
2023-03-23 16:29:37 -0300

    Inicio

---
+++ App.tsx
@@ -0,0 +1,68 @@
+import React, {useState} from 'react';
+import {View, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import lightOn from './assets/icons/eco-light.png';
+import lightOff from './assets/icons/eco-light-off.png';
+
+import logoDioWhite from './assets/icons/logo-dio-white.png';
+import logoDio from './assets/icons/logo-dio.png';
+
+const App = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleChangeToggle = () => {
+    setToggle(!toggle);
+  };
+
+  return (
+    <View style={toggle ? styles.containerLight : styles.container}>
+      <TouchableOpacity onPress={handleChangeToggle}>
+        <Image
+          style={toggle ? styles.lightingOn : styles.lightingOff}
+          source={toggle ? lightOn : lightOff}
+        />
+
+        <Image
+          style={styles.dioLogo}
+          source={toggle ? logoDio : logoDioWhite}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  containerLight: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  lightingOn: {
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    width: 150,
+    height: 150,
+  },
+  lightingOff: {
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    tintColor: 'white',
+    width: 150,
+    height: 150,
+  },
+  dioLogo: {
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    width: 250,
+    height: 250,
+  },
+});
